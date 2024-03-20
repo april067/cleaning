383549ffd41a2556e0d9c9cbccaaeeb65db296ba
april067 <143269646+april067@users.noreply.github.com>
2024-03-20 12:50:21 +0000

    Deploying to gh-pages from @ april067/cleaning@823d3f08de0e116d607001ab8180ebe4395832b2 🚀

---
+++ index.2ede69b6.js
@@ -0,0 +1,2 @@
+console.log("// mob-menu file");const e=document.getElementById("burger"),n=document.getElementById("nav-list");document.getElementById("burger-label").addEventListener("click",function(){e.checked||n.classList.add("nav__open"),e.checked&&n.classList.remove("nav__open")});
+//# sourceMappingURL=index.2ede69b6.js.map
---
+++ index.dbd83109.js
@@ -0,0 +1,2 @@
+!function(){console.log("// mob-menu file");let e=document.getElementById("burger"),n=document.getElementById("nav-list");document.getElementById("burger-label").addEventListener("click",function(){e.checked||n.classList.add("nav__open"),e.checked&&n.classList.remove("nav__open")})}();
+//# sourceMappingURL=index.dbd83109.js.map
