b68082398d16e9aa9c7c478161b1c51f3630446a
Iker King <ik@ik.local>
2024-03-21 06:24:30 +0200

    hero

--- src/js/mob-menu.js
+++ src/js/mob-menu.js
@@ -3,14 +3,17 @@ console.log('// mob-menu file');
 const burger = document.getElementById('burger');
 const nav = document.getElementById('nav-list');
 const label = document.getElementById('burger-label');
+const body = document.querySelector('body');
 
 label.addEventListener('click', toggle);
 
 function toggle() {
   if (!burger.checked) {
     nav.classList.add('nav__open');
+    body.classList.add('menu-open');
   }
   if (burger.checked) {
     nav.classList.remove('nav__open');
+    body.classList.remove('menu-open');
   }
 }
